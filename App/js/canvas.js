befd1be0d2a925dca79db93f8fee2177f41354d8
Marcos Lazaro ALvarez Arteaga <marcosalv92@gmail.com>
2021-04-01 14:13:28 -0400

    Pintando con Canvas y Teclado

--- App/js/canvas.js
+++ App/js/canvas.js
@@ -1,4 +1,11 @@
 // var box = prompt("Holaaaa");
+var paint_2D = document.getElementById("paint");
+var lienzo = paint_2D.getContext("2d");
+var xActual = parseInt(paint_2D.width)/2;
+var yActual = parseInt(paint_2D.height)/2;
+
+
+
 
 var teclas = {
     UP : 38,
@@ -16,18 +23,27 @@ function paintKeydown(evento)
     
     switch (evento.keyCode) {
         case teclas.UP:
+            
+        paintLine("blue",xActual,yActual,xActual,yActual-10)
+            yActual = yActual - 10;    
             console.log("Arriba");       
         break;
         
         case teclas.DOWN:
+            paintLine("blue",xActual,yActual,xActual,yActual+10)
+            yActual = yActual + 10;
             console.log("Abajo");
         break;
         
         case teclas.LEFT:
+            paintLine("blue",xActual,yActual,xActual-10,yActual)
+            xActual = xActual - 10;
             console.log("Izquierda");
         break;
         
         case teclas.RIGHT:
+            paintLine("blue",xActual,yActual,xActual+10,yActual)
+            xActual = xActual + 10;
             console.log("Derecha");
         break;
 
@@ -36,4 +52,16 @@ function paintKeydown(evento)
             console.log("Otra Tecla");
             break;
     }
+}
+
+function paintLine(color, xinicial, yinicial, xfinal, yfinal)
+{
+    lienzo.beginPath();
+    lienzo.strokeStyle = color;
+    lienzo.moveTo(xinicial,yinicial);
+    lienzo.lineTo(xfinal,yfinal);
+    lienzo.stroke();    
+    lienzo.closePath();
+ 
+    
 }
